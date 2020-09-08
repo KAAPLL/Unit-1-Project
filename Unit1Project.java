67b3ce11db6a3179eeeb136fb3f172dd2a7469ec
KAAPLL <69614761+KAAPLL@users.noreply.github.com>
2020-09-07 20:08:20 -0700

    Add files via upload

--- Unit1Project.java
+++ Unit1Project.java
@@ -3,7 +3,8 @@ import java.math.*;
 public class Unit1Project {
 	//bunch of variables
 	static int f = 0;
-	static String[][] phase= {{" ", " ", " "}, {" starting ", " final "},{"heating ","cooling "}};
+	static boolean endothermic = true;
+	static String[][] phase= {{" ", " ", " "}, {" starting ", " final "},{"heating ","cooling "}, {"solid", "liquid", "gas"}};
 	static double[] specificHeats = {0.002108, 0.004184, 0.001996};
 	static double[][] tempBoundaries = {{-273.14, 0}, {0, 100}, {100, 700}};
 	static double[] latentHeat = {0.333, 2.257};
@@ -11,40 +12,35 @@ public class Unit1Project {
 	static double massOfWater = 0;
 	static double energyInFunc = 0;
 	static double energyAdded = 0;
-	//all changes within phases
-	public static double roundTo3(double value) {
+	//round to 3 decimal places
+	static double roundTo3(double value) {
 	    BigDecimal bd = new BigDecimal(Double.toString(value));
 	    bd = bd.setScale(3, RoundingMode.HALF_UP);
 	    return bd.doubleValue();
 	}
-	static double TempChanges(double specificHeats[],double temp[],double massOfWater, double energyAdded, String phase[][], double energyInFunc) {
+	//all changes within phases
+	static double TempChanges() {
 		double tempChange = 0;
-		if (phase[0][2].equals("solid")) {f = 0;}
-		if (phase[0][2].equals("liquid")) {f = 1;}
-		if (phase[0][2].equals("gas")) {f = 2;}
+		if (phase[0][2].equals(phase[3][0])) {f = 0;}
+		if (phase[0][2].equals(phase[3][1])) {f = 1;}
+		if (phase[0][2].equals(phase[3][2])) {f = 2;}
 		if (temp[0]<temp[1]) {tempChange=roundTo3((Math.min(temp[1], tempBoundaries[f][1])-Math.max(temp[0], tempBoundaries[f][0])));}
 		if (temp[0]>temp[1]) {tempChange=roundTo3((Math.max(temp[1],tempBoundaries[f][0])-Math.min(temp[0], tempBoundaries[f][1])));}
 		temp[2]+=tempChange;
 		energyInFunc = roundTo3(specificHeats[f]*tempChange*massOfWater);
 		return energyInFunc;}
-	//solid-liquid, vice versa
-	static double SolidPhaseChange(double latentHeat[], double temp[], double massOfWater, double energyAdded, String phase[][], double energyInFunc) {
-		energyInFunc = roundTo3(latentHeat[0] * massOfWater);
-		phase[0][2] = "liquid";
-		if (temp[2]>temp[1]) { 
+	//all phase changes
+	static double PhaseChanges() {
+		if (temp[2] == 0) { f = 0;}
+		if (temp[2] == 100) { f = 1;}
+		energyInFunc = roundTo3(latentHeat[f] * massOfWater);
+		if (endothermic == true) {phase[0][2] = phase[3][f+1];}
+		else {		
+			phase[0][2] = phase[3][f];
 			energyInFunc *= -1;
-			phase[0][2] = "solid";
 		}
 		return energyInFunc;}
-	//liquid-gas, vice versa
-	static double GasPhaseChange(double latentHeat[], double temp[], double massOfWater, double energyAdded, String phase[][], double energyInFunc) {
-		energyInFunc = roundTo3(latentHeat[1] * massOfWater);
-		phase[0][2] = "gas";
-		if (temp[2]>temp[1]) { 
-			energyInFunc *= -1;
-			phase[0][2] = "liquid";
-		}
-		return energyInFunc;}
+
 	public static void main(String[] args) {
 		double Energy = 0;
 		Scanner sc = new Scanner(System.in);
@@ -57,33 +53,31 @@ public class Unit1Project {
 		sc.close();
 		temp[2]=temp[0];
 		for (int i=0; i<2; i++) {
-			if (temp[i] < 0) {phase[0][i] = "solid";}
-			if (temp[i] >= 0 && temp[0] <= 100) {phase[0][i] = "liquid";}
-			if (temp[i] > 100) {phase[0][i] = "gas";}}
+			if (temp[i] < 0) {phase[0][i] = phase[3][0];}
+			if (temp[i] >= 0 && temp[0] <= 100) {phase[0][i] = phase[3][1];}
+			if (temp[i] > 100) {phase[0][i] = phase[3][2];}}
 		phase[0][2]=phase[0][0];
 		String p1 = "We will be ";
 		String p2 = phase[2][0];
-		if (temp[0]>temp[1]) {p2 = phase[2][1];}
+		if (temp[0]>temp[1]) {
+			p2 = phase[2][1];
+			endothermic = false;
+		}
 		String p3 = " grams of water from ";
 		String p4 = " Celsius(";
 		String p5 = ") to ";
 		String p6 = ").";
 		System.out.println(p1+p2+massOfWater+p3+temp[0]+p4+phase[0][0]+p5+temp[1]+p4+phase[0][1]+p6);
 		while (true) {
-			energyAdded = TempChanges(specificHeats,temp,massOfWater, energyAdded,phase, energyInFunc);
+			energyAdded = TempChanges();
 			if (energyAdded != 0) {
 				Energy += energyAdded;
 				System.out.println(p2+"("+phase[0][2]+"): " +energyAdded+" kJ");
 			}
-			if ((phase[0][2].equals("solid") && temp[2]<temp[1])||(phase[0][2].equals("liquid") && temp[1]<temp[2])) {
-				energyAdded = SolidPhaseChange(latentHeat, temp, massOfWater, energyAdded, phase, energyInFunc);
-				System.out.println("Changed phase to "+phase[0][2]+": "+energyAdded+" kJ.");
-				Energy += energyAdded;
-			}
-			else if ((phase[0][2].equals("liquid") && temp[2]<temp[1])||(phase[0][2].equals("gas") && temp[1]<temp[2])) {
-				energyAdded = GasPhaseChange(latentHeat, temp, massOfWater, energyAdded, phase, energyInFunc);
-				System.out.println("Changed phase to "+phase[0][2]+": "+energyAdded+" kJ.");
-				Energy += energyAdded;
+			if (phase[0][2] != phase[0][1]) {
+				energyAdded = PhaseChanges();
+				System.out.println("Changed phase to "+phase[0][2]+": "+energyAdded+" kJ");
+				Energy+=energyAdded;
 			}
 			if (temp[1]==temp[2]) {break;}
 		}
